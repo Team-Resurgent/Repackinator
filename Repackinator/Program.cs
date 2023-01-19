f7679d836e4470aed2349a9595b636742c4c7421
EqUiNoX <87106037+EqUiNoX-Labs@users.noreply.github.com>
2023-01-19 14:41:49 -0800

    Update Program.cs

--- Repackinator/Program.cs
+++ Repackinator/Program.cs
@@ -3,7 +3,7 @@ using Repackinator.Shell;
 using Repackinator.Console;
 using Repackinator.UI;
 
-var version = "v1.2.9";
+var version = "v1.2.8";
 
 ContextMenu.RegisterContext();
 CultureInfo.DefaultThreadCurrentUICulture = Thread.CurrentThread.CurrentCulture;
