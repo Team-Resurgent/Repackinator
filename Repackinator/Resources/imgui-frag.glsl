99a351b510c492c910e81f23d4be0986310565e0
EqUiNoX <equinox.teamresurgent@gmail.com>
2023-05-23 10:10:52 -0700

    Update: shader

--- Repackinator/Resources/imgui-frag.glsl
+++ Repackinator/Resources/imgui-frag.glsl
@@ -1,13 +1,13 @@
-#version 130
-precision mediump float;
-precision mediump int;
+#version 330 core
 
 uniform sampler2D in_fontTexture;
 
-varying vec4 color;
-varying vec2 texCoord;
+in vec4 color;
+in vec2 texCoord;
+
+out vec4 outputColor;
 
 void main()
 {
-    gl_FragColor = color * texture(in_fontTexture, texCoord);
+    outputColor = color * texture(in_fontTexture, texCoord);
 }
