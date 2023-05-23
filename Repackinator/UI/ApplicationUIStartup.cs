32c6e91600cc0686443fae160647075453e51c99
EqUiNoX <equinox.teamresurgent@gmail.com>
2023-05-23 13:41:00 -0700

    Update: removed hide

--- Repackinator/UI/ApplicationUIStartup.cs
+++ Repackinator/UI/ApplicationUIStartup.cs
@@ -4,24 +4,10 @@ namespace Repackinator.UI
 {
     public static class ApplicationUIStartup
     {
-        [DllImport("kernel32.dll")]
-        private static extern IntPtr GetConsoleWindow();
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        private static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
-
-        private const int WM_CLOSE = 0x0010;
-
         public static void Start(string version)
         {
             try
             {
-                if (OperatingSystem.IsWindows())
-                {
-                    var handle = GetConsoleWindow();
-                    SendMessage(handle, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
-                }
-
                 var application = new ApplicationUI(version);
                 application.Run();
             }
