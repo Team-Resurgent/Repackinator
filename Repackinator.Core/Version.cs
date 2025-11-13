fc6b0f78274ad87a91349c42e08ab7dae23252a5
EqUiNoX <87106037+EqUiNoX-Labs@users.noreply.github.com>
2025-11-12 20:13:12 -0700

    Update Version.cs

--- Repackinator.Core/Version.cs
+++ Repackinator.Core/Version.cs
@@ -8,6 +8,6 @@ namespace Repackinator.Core
 {
     public static class Version
     {
-        public static string Value { get; } = "2.0.1";
+        public static string Value { get; } = "2.0.2";
     }
 }
